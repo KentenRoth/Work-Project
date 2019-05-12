cdbfa60a70c2de85bae1915e643d1d0389d1ca72
Kent Roth <kentencroth@yahoo.com>
2019-05-12 16:09:07 -0500

    WIP: Added form validation to AddCar

--- src/Components/AddCar/AddCar.js
+++ src/Components/AddCar/AddCar.js
@@ -1,35 +1,67 @@
 import React, { Component } from 'react';
 import './CSS/AddCar.css';
 
+function validate(keytag, year, make, model, service) {
+	const errors = [];
+	const keytagRegex = new RegExp(/([0-9]{6})/g);
+	const yearRegex = new RegExp(/([0-9]{4})/g);
+	if (keytagRegex.test(keytag) === false) {
+		errors.push('Keytag must be 6 numbers');
+	}
+	if (yearRegex.test(year) === false) {
+		errors.push('Year needs to be formatted like YYYY');
+	}
+	if (make.length === 0) {
+		errors.push('Please enter the Make of the vehicle ');
+	}
+	if (model.length === 0) {
+		errors.push('Please enter the Model of the vehicle');
+	}
+	if (service.length === 0) {
+		errors.push('Please enter the requested service');
+	}
+
+	return errors;
+}
+
 class AddCar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
+			keytag: '',
 			year: '',
 			make: '',
 			model: '',
-			service: ''
+			service: '',
+
+			errors: []
 		};
 	}
 
-	handleSubmit(e) {
+	handleSubmit = e => {
 		e.preventDefault();
-		const name = e.target.name;
-		this.setState({
-			[name]: e.target.value
-		});
-	}
+
+		const { keytag, year, make, model, service } = this.state;
+
+		const errors = validate(keytag, year, make, model, service);
+		if (errors.length > 0) {
+			this.setState({ errors });
+			return;
+		}
+	};
 
 	handleChange = e => {
 		const name = e.target.name;
+		const value = e.target.value;
 		this.setState({
-			[name]: e.target.value
+			[name]: value
 		});
 	};
 
 	handleClear = () => {
 		this.setState({
+			keytag: '',
 			year: '',
 			make: '',
 			model: '',
@@ -38,10 +70,23 @@ class AddCar extends Component {
 	};
 
 	render() {
+		const { errors } = this.state;
 		return (
 			<div className="container">
 				<div className="box">
-					<form className="form">
+					{errors.map(error => (
+						<p key={error}>Error: {error}</p>
+					))}
+
+					<form className="form" onSubmit={this.handleSubmit}>
+						<input
+							className="keytag"
+							type="number"
+							value={this.state.keytag}
+							onChange={this.handleChange}
+							name="keytag"
+							placeholder="Keytag ex. 123456"
+						/>
 						<input
 							className="year"
 							type="number"
