94cd50cc16288059bcf0ff88a2abd4852caf9e6c
Kent Roth <kentencroth@yahoo.com>
2019-05-17 18:02:22 -0500

    WIP: Posting to DB is working!

--- Server/routes/api/cars.js
+++ Server/routes/api/cars.js
@@ -7,4 +7,22 @@ router.get('/', (req, res) => {
 	Car.find().then(cars => res.json(cars));
 });
 
+router.post('/', (req, res) => {
+	const newCar = new Car({
+		keytag: req.body.keytag,
+		year: req.body.year,
+		make: req.body.make,
+		model: req.body.model,
+		service: req.body.service
+	});
+	newCar.save().then(
+		car => {
+			res.send(car);
+		},
+		e => {
+			res.status(400).send(e);
+		}
+	);
+});
+
 module.exports = router;
