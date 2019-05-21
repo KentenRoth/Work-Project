b1eb77447de31498b3466593baef588ee04d7b8c
Kent Roth <kentencroth@yahoo.com>
2019-05-20 19:24:43 -0500

    WIP: Fixed Typo

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-is('should return true', () => {
+it('should return true', () => {
 	expect(true).toBe(true);
 });
